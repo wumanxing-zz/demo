3fd854be67154191cc6275a3146540cc782f76bd
wumanxing <wumanxing123@163.com>
2016-04-11 19:35:30 +0800

    测试

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 
 		System.out.print("test");
+		System.out.print("test");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
